58e8c5a86a779abac3eee4c7429e3ca8993eb378
Anthony Schmitt <35774930+schmittaj@users.noreply.github.com>
2018-04-09 17:32:09 -0500

    Update NESreader.java
    Check to make sure file is mapper 0 and it's attributes conform to that. Also fixed a few attributes (the boolean ones) that weren't being properly set.

--- src/core/NESreader.java
+++ src/core/NESreader.java
@@ -24,7 +24,7 @@ import java.io.*;
  * 			7-4					Lower mapper number
  * 			3					1 = ignore mirroring control or mirroring bit, provides 4-screen VRAM
  * 			2					1 = 512 byte trainer to go at $7000-$71FF, stored in file before PRG data
- * 			1					1 = Battery-backed PGR RAM goes at $6000-$7FFF or other persistant memory
+ * 			1					1 = Battery-backed PGR RAM goes at $6000-$7FFF or other persistent memory
  * 			0					Mirroring 0 = Horizontal (vertical mode); 1 = Vertical (horizontal mode)
  * 7		BITS (76543210)		CONTENTS
  * 			7-4					Upper mapper number
@@ -85,29 +85,90 @@ public class NESreader
 				prgROMblocks = header[4];
 				chrROMblocks = header[5];
 				mapperNumber = ((header[6] & 0xf0)>>4)|(header[7] & 0xf0);
-				ignoreMirroring = Boolean.parseBoolean(Integer.toString(header[6]&0x08));
-				trainer = Boolean.parseBoolean(Integer.toString(header[6]&0x04));
-				batteryBacked = Boolean.parseBoolean(Integer.toString(header[6]&0x02));
+				
+				ignoreMirroring = ((header[6]&0x08)>>3)==1;
+				trainer = ((header[6]&0x04)>>2)==1;
+				batteryBacked = ((header[6]&0x02)>>1)==1;
 				mirroring = header[6]&0x01;
 				if( (header[7]&0x0c) == 2)
 				{
 					nes2 = true;
 				}
-				playChoice = Boolean.parseBoolean(Integer.toString(header[7]&0x02));
-				vsUni = Boolean.parseBoolean(Integer.toString(header[7]&0x01));
+				playChoice = ((header[7]&0x02)>>1)==1;
+				vsUni = (header[7]&0x01)==1;
 				prgRAM = header[8];
 				videoRegion = header[9]&0x01;
 
 				//for the moment ignoring trainer and battery backed;
-				
-				for(int a = 0; a < (0x4000 * prgROMblocks); a++) //as is this is assuming no more than 2 blocks
+				if(mapperNumber != 0)
 				{
-					nes.CPUmemory[0x8000+a] = (byte) fileIn.read();
+					System.out.println("Mapper " + mapperNumber + " not supported.");
 				}
-				
-				for(int a = 0; a < (0x2000 * chrROMblocks); a++) //as is this is assuming no more than 1 block
+				else if(!(prgROMblocks == 2 | prgROMblocks == 4))
 				{
-					nes.PPUmemory[a] = (byte) fileIn.read();
+					System.out.println("Bad PRG ROM size");
+				}
+				else if(chrROMblocks != 1)
+				{
+					System.out.println("Bad CHR size");
+				}
+				else if(ignoreMirroring != false)
+				{
+					System.out.println("Unsupported mirroring.");
+				}
+				else if(trainer != false)
+				{
+					System.out.println("Unsupported trainer.");
+				}
+				else if(playChoice != false)
+				{
+					System.out.println("PlayChoice not supported.");
+				}
+				else if(vsUni != false)
+				{
+					System.out.println("Vs Unisystem not supported.");
+				}
+				else if(videoRegion != 0)
+				{
+					System.out.println("Only NTSC supported.");
+				}
+				else if(prgRAM > 1)
+				{
+					System.out.println("PRG RAM too large.");
+				}
+				else
+				{
+					/*
+					 * Mapper 0 supports either 2 or 4 K PRG RAM not the normal 8k.
+					 * If the PRG ROM is 16 then 2k of PRG RAM, 32k is 4K PRG RAM.
+					 * Either way the leftover space in $6000 - $7fff is filled with
+					 * mirrored data. 
+					 */
+					if(batteryBacked) 
+					{
+						int loop = (8/prgROMblocks) - 1;
+						for(int a = 0; a < (0x0800 * (prgROMblocks/2)); a++)
+						{
+							nes.CPUmemory[0x6000+a] = (byte) fileIn.read();
+						}
+						for(int a = 0x6800; loop > 0; loop--)
+						{
+							for(int b = 0x6000; b < 0x6800; b++)
+							{
+								nes.CPUmemory[a++] = nes.CPUmemory[b];
+							}
+						}
+						
+					}
+					for(int a = 0; a < (0x4000 * prgROMblocks); a++) //as is this is assuming no more than 2 blocks
+					{
+						nes.CPUmemory[0x8000+a] = (byte) fileIn.read();
+					}
+					
+					for(int a = 0; a < (0x2000 * chrROMblocks); a++)
+					{
+						nes.PPUmemory[a] = (byte) fileIn.read();
+					}
 				}
 			}
 			
